1281fd29c57a5710c62426fb3306a12cd577ee1a
Ivan Kasenka <kasenka.ivan@gmail.com>
2023-01-05 02:53:21 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(result) {
+
+    result = Math.abs(result).toString();
+    result = reverseInt(result);
+
+    function reverseInt(num) {
+        num = deleteZero(num);
+        let splitString = num.split('');
+        let reverseArr = splitString.reverse()
+        let joinArray = reverseArr.join('');
+        result = joinArray;
+        return result;
+    }
+
+    function deleteZero(num) {
+        if (num[num.length - 1] == '0') {
+            num = num.slice(0, -1);
+            deleteZero(num);
+        }
+        return num;
+    }
+
+    return result;
 }
